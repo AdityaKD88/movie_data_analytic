131dca0abfb10a4fd52dd5e4004af45a214a472a
Aditya Kumar <ak9935699605@gmail.com>
2022-05-03 18:29:52 +0530

    flask

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 
 df=pd.read_csv('IMDB-Movie-Data.csv')
 
+def count_genre(x):
+    data_plot = df[x].str.cat(sep = ',')
+    data = pd.Series(data_plot.split(','))
+    info = data.value_counts(ascending=False)
+    return info
+
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -68,8 +74,90 @@ def top():
   return render_template('top.html',graph1=graph1,graph2=graph2,graph3=graph3,graph4=graph4)
 
 @app.route('/year')
-def index():
-  return render_template('year.html')
+def year():
+  x=df['Year'].sort_values().unique()
+  y=df.groupby('Year').count()['Title']
+  fig5 = px.bar(df, x=x, y=y, color=x, title='Year Vs Movie Release',
+                  labels={
+                            'x':'Year',
+                            'y':'Count'
+                        })
+  graph5 = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
+
+  yr = df['Year'].value_counts()
+  fig6 = px.pie(df,names=yr.index,values=yr.values,title='Percentage of movies released per year')
+  graph6 = json.dumps(fig6, cls=plotly.utils.PlotlyJSONEncoder)
+
+  x=df['Year'].sort_values().unique()
+  y=df.groupby('Year').mean()['Revenue (Millions)']
+  fig7 =px.bar(df, x=x, y=y, color=x, title='Year Vs Mean Revenue',
+                labels={
+                          'x':'Year',
+                          'y':'Average Revenue'
+                      })
+  graph7 = json.dumps(fig7, cls=plotly.utils.PlotlyJSONEncoder)
+
+  return render_template('year.html',graph5=graph5,graph6=graph6,graph7=graph7)
+
+@app.route('/rate')
+def rate():
+  rating = df['Rating'].value_counts()
+  fig8 = px.bar(df, x=rating.index.values, y=rating.values, color=rating.index.values,
+                title='Rating count of movies',
+                labels={'x':'Rating',
+                        'y':'No. of Movies'})
+  graph8 = json.dumps(fig8, cls=plotly.utils.PlotlyJSONEncoder)
+
+  m_score = df['Metascore'].value_counts()
+  fig9 = px.bar(df, x=m_score.index.values, y=m_score.values, color=m_score.index.values,
+                title='Metascore count of movies',
+                labels={'x':'Metascore',
+                        'y':'No. of Movies'})
+  graph9 = json.dumps(fig9, cls=plotly.utils.PlotlyJSONEncoder)
+
+  fig10 = px.scatter(df,x=df['Rating'].values,y=df['Title'].values,
+            title='Rating of different movies',color=df['Rating'].values,
+            height=1600, labels={
+                'x':'Rating',
+                'y':'Movie Name'
+            })
+  graph10 = json.dumps(fig10, cls=plotly.utils.PlotlyJSONEncoder)
+
+  return render_template('rate.html',graph8=graph8,graph9=graph9,graph10=graph10)
+
+@app.route('/genre')
+def genre():
+  gen=count_genre('Genre')
+  fig11 = px.bar(df, x=gen.values, y=gen.index, orientation='h',color=gen.index,
+              title='Genre with Highest release',
+              labels={'x':'No. of Movies',
+                      'y':'Genre'})
+  graph11 = json.dumps(fig11, cls=plotly.utils.PlotlyJSONEncoder)
+
+  fig12 = px.pie(df, names=gen.index, values=gen.values, title='Percentage of genre of movies')
+  graph12 = json.dumps(fig12, cls=plotly.utils.PlotlyJSONEncoder)
+
+  return render_template('genre.html',graph11=graph11,graph12=graph12)
+
+@app.route('/act')
+def act():
+  actors = count_genre('Actors')
+  n=actors.iloc[:20]
+  fig13 = px.bar(df,y=n.values, x=n.index,color=n.index,
+          title='Most Frequent Actor',
+          labels={'x':'Actor Name',
+                  'y':'No. of Movies'})
+  graph13 = json.dumps(fig13, cls=plotly.utils.PlotlyJSONEncoder)
+
+  director = count_genre('Director')
+  n=director.iloc[:20]
+  fig14 = px.bar(df,y=n.values, x=n.index,color=n.index,
+          title='Most Frequent Director',
+          labels={'x':'Director Name',
+                  'y':'No. of Movies'})
+  graph14 = json.dumps(fig14, cls=plotly.utils.PlotlyJSONEncoder)
+
+  return render_template('act.html',graph13=graph13,graph14=graph14)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
